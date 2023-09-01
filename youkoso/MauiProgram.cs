6626404b5e10a527f580957caba606ec418beb98
Bashir Ahmed <bashir963@yahoo.co.jp>
2023-09-01 10:52:18 +0900

    初回コミットメッセージ

---
+++ youkoso/MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
+using youkoso.ViewModels;
+using youkoso.Views;
+
+namespace youkoso
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+.UseMauiApp<App>()
+.UseMauiCommunityToolkit()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            builder.UseMauiCommunityToolkitMediaElement(); // ここにメソッドを追加
+
+            return builder.Build();
+        }
+    }
+}
